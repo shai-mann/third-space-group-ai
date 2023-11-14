49cf8840a4faa501d5f82051680440b376bdce68
Karim Semaan <91396847+karimsemaan@users.noreply.github.com>
2023-11-14 17:58:20 -0500

    changes
    added variables to .env. Fixed structure of main.py and db.py

--- src/db.py
+++ src/db.py
@@ -1,6 +1,50 @@
 """
 This is where database interface code goes
 """
+import psycopg2
+from psycopg2 import sql
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+import os
+from dotenv import load_dotenv
+
+dotenv_path = os.path.join(os.path.dirname(__file__), "..",'.env.dev')
+
+load_dotenv(dotenv_path=dotenv_path)
+
+# Database parameters
+db_params = {
+    'dbname': os.environ.get('DB_NAME'),
+    'user': os.environ.get('DB_USER'), 
+    'password': os.environ.get('DB_PASSWORD'),   
+    'host': os.environ.get('DB_HOST'),   
+    'port': os.environ.get('DB_PORT'),  
+}
+
+# Establishing a connection to the database
+def connect_db():
+    try:
+        conn = psycopg2.connect(**db_params)
+        conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+        return conn
+    except Exception as e:
+        print(f"Error connecting to the database: {e}")
+        return None
+
+def execute_query(query, args=None):
+    conn = connect_db()
+    if conn:
+        try:
+            with conn.cursor() as cur:
+                cur.execute(query, args)
+                if cur.description:  # Check if it's a SELECT query
+                    return cur.fetchall()
+                conn.commit()
+        except Exception as e:
+            print(f"Error executing query: {e}")
+        finally:
+            conn.close()
+    return None
+
 
 class Database():
     """
@@ -27,3 +71,5 @@ class Database():
     
     def getGroupMembers(self):
         pass
+
+
--- src/main.py
+++ src/main.py
@@ -1,41 +1,4 @@
-import psycopg2
-from psycopg2 import sql
-from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
-
-# Database connection parameters
-db_params = {
-    'dbname': 'postgres', 
-    'user': 'postgres',    
-    'password': '12345',   
-    'host': 'localhost',   
-    'port': '5432'         
-}
-
-
-# Establishing a connection to the database
-def connect_db():
-    try:
-        conn = psycopg2.connect(**db_params)
-        conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
-        return conn
-    except Exception as e:
-        print(f"Error connecting to the database: {e}")
-        return None
-
-def execute_query(query, args=None):
-    conn = connect_db()
-    if conn:
-        try:
-            with conn.cursor() as cur:
-                cur.execute(query, args)
-                if cur.description:  # Check if it's a SELECT query
-                    return cur.fetchall()
-                conn.commit()
-        except Exception as e:
-            print(f"Error executing query: {e}")
-        finally:
-            conn.close()
-    return None
+from db import connect_db
 
 def main():
     # Connect to the database
