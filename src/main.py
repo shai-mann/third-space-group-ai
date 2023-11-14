f717eee1f6f71bbf7c2a6fe56f90616ad8563fb8
Karim Semaan <91396847+karimsemaan@users.noreply.github.com>
2023-11-14 13:56:07 -0500

    change main file to connect to postgres db

--- src/main.py
+++ src/main.py
@@ -1,6 +1,50 @@
+import psycopg2
+from psycopg2 import sql
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+
+# Database connection parameters
+db_params = {
+    'dbname': 'postgres', 
+    'user': 'postgres',    
+    'password': '12345',   
+    'host': 'localhost',   
+    'port': '5432'         
+}
+
+
+# Establishing a connection to the database
+def connect_db():
+    try:
+        conn = psycopg2.connect(**db_params)
+        conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+        return conn
+    except Exception as e:
+        print(f"Error connecting to the database: {e}")
+        return None
+
+def execute_query(query, args=None):
+    conn = connect_db()
+    if conn:
+        try:
+            with conn.cursor() as cur:
+                cur.execute(query, args)
+                if cur.description:  # Check if it's a SELECT query
+                    return cur.fetchall()
+                conn.commit()
+        except Exception as e:
+            print(f"Error executing query: {e}")
+        finally:
+            conn.close()
+    return None
+
 def main():
-    # do something
-    print("hello")
+    # Connect to the database
+    conn = connect_db()
+    if conn:
+        print("Connected to the database successfully.")
+        conn.close()
+    else:
+        print("Failed to connect to the database.")
 
 
 if __name__ == "__main__":
