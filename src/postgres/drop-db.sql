1ba8a319f5f0e00ec6e0ac8a9fc768cff29d62d6
Shai Mann-Robison <mann.robison@gmail.com>
2023-11-14 12:22:55 -0500

    create schema files

---
+++ src/postgres/drop-db.sql
@@ -0,0 +1,10 @@
+-- DROPS ALL DATA IN THE DATABASE
+-- This file exists in case a dev wants to reset their database
+
+drop table if exists group_members;
+drop table if exists groups;
+
+drop table if exists user_hobbies;
+drop table if exists user_friends;
+drop table if exists users;
+drop table if exists hobbies;
