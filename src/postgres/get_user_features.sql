00a7f6b3989d2742418f966d201d96d27a1c8eca
Karim Semaan <91396847+karimsemaan@users.noreply.github.com>
2023-12-08 12:21:33 -0500

    small error fixed where I did not account for both ways

--- src/postgres/get_user_features.sql
+++ src/postgres/get_user_features.sql
@@ -20,38 +20,26 @@ BEGIN
 
     -- Check if the user is a friend with the central user and cast to integer
     SELECT (CASE WHEN EXISTS (
-        SELECT 1 FROM user_friends uf WHERE uf."user" = user_id AND uf.friend = 0
+        SELECT 1 FROM user_friends uf WHERE (uf."user" = user_id AND uf.friend = 0) OR (uf."user" = 0 AND uf.friend = user_id)
     ) THEN 1 ELSE 0 END) INTO is_friend;
 
     -- Calculate the number of common friends with the central user
-    SELECT COUNT(*) INTO common_friends_count
+    SELECT COUNT(DISTINCT uf1.friend) INTO common_friends_count
     FROM user_friends uf1
     JOIN user_friends uf2 ON uf1.friend = uf2.friend
-    WHERE uf1."user" = user_id AND uf2."user" = 0;
+    WHERE (uf1."user" = user_id AND uf2."user" = 0) OR (uf1."user" = 0 AND uf2."user" = user_id);
 
     -- Get the count of common hobbies with the central user
-    SELECT COUNT(*) INTO common_hobbies_count
-    FROM (
-        SELECT uh.hobby
-        FROM user_hobbies uh
-        WHERE uh."user" = user_id
-        INTERSECT
-        SELECT uh.hobby
-        FROM user_hobbies uh
-        WHERE uh."user" = 0
-    ) AS common_hobbies;
+    SELECT COUNT(DISTINCT uh.hobby) INTO common_hobbies_count
+    FROM user_hobbies uh
+    JOIN user_hobbies uh0 ON uh.hobby = uh0.hobby AND uh0."user" = 0
+    WHERE uh."user" = user_id;
 
     -- Get the count of common groups with the central user
-    SELECT COUNT(*) INTO common_groups_count
-    FROM (
-        SELECT gm.group
-        FROM group_members gm
-        WHERE gm."user" = user_id
-        INTERSECT
-        SELECT gm.group
-        FROM group_members gm
-        WHERE gm."user" = 0
-    ) AS common_groups;
+    SELECT COUNT(DISTINCT gm.group) INTO common_groups_count
+    FROM group_members gm
+    JOIN group_members gm0 ON gm.group = gm0.group AND gm0."user" = 0
+    WHERE gm."user" = user_id;
 
     RETURN NEXT;
 END; $$
